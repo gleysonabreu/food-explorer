e22219faae1c78de3563788772838e71f91b14cc
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-02-24 16:35:44 -0300

    add outline color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export function Button({ children, ...rest }: ButtonProps) {
   return (
     <button
       {...rest}
-      className="px-8 py-3 flex flex-row items-center justify-center gap-2 bg-food-tints-tomato-100 hover:bg-food-tints-tomato-200 disabled:bg-food-tints-tomato-400 disabled:cursor-not-allowed transition-colors rounded-md text-food-light-100 font-poppins font-medium text-sm"
+      className="px-8 py-3 flex flex-row items-center justify-center gap-2 bg-food-tints-tomato-100 hover:bg-food-tints-tomato-200 disabled:bg-food-tints-tomato-400 disabled:cursor-not-allowed transition-colors rounded-md text-food-light-100 font-poppins font-medium text-sm focus:outline-none focus:ring-2 focus:ring-food-tints-tomato-100 focus:ring-offset-2 focus:ring-offset-food-dark-400"
     >
       {children}
     </button>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,7 +6,7 @@ export function Input({ ...rest }: InputProps) {
   return (
     <input
       {...rest}
-      className="w-full bg-food-dark-900 rounded-lg h-12 px-4 py-3 placeholder:text-food-light-500 outline-none text-food-light-100"
+      className="w-full bg-food-dark-900 rounded-lg h-12 px-4 py-3 placeholder:text-food-light-500 text-food-light-100 focus:outline-none focus:ring-2 focus:ring-food-tints-cake-200 focus:ring-offset-2 focus:ring-offset-food-dark-400"
     />
   );
 }
