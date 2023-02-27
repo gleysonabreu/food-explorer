6716f2fe5e833c945effc2f513530fc41bfeca40
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-02-27 09:55:25 -0300

    change z-index from 50 to 20

--- src/components/NextArrowSlider.tsx
+++ src/components/NextArrowSlider.tsx
@@ -3,7 +3,7 @@ import { CaretRight } from "./Icons/CaretRight";
 
 export function NextArrowSlider({ onClick }: CustomArrowProps) {
   return (
-    <div className="w-56 z-50 h-full top-0 right-0 absolute flex items-center justify-end bg-gradient-food-caret-right">
+    <div className="w-56 z-20 h-full top-0 right-0 absolute flex items-center justify-end bg-gradient-food-caret-right">
       <button onClick={onClick}>
         <CaretRight />
       </button>
--- src/components/PrevArrowSlider.tsx
+++ src/components/PrevArrowSlider.tsx
@@ -3,7 +3,7 @@ import { CaretRight } from "./Icons/CaretRight";
 
 export function PrevArrowSlider({ onClick }: CustomArrowProps) {
   return (
-    <div className="matrix-caret-left w-56 transform-[matrix(-1, 0, 0, 1, 0, 0)] z-50 h-full top-0 left-0 absolute flex items-center justify-end bg-gradient-food-caret-right">
+    <div className="matrix-caret-left w-56 transform-[matrix(-1, 0, 0, 1, 0, 0)] z-20 h-full top-0 left-0 absolute flex items-center justify-end bg-gradient-food-caret-right">
       <button onClick={onClick}>
         <CaretRight />
       </button>
