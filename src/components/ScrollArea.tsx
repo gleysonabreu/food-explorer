22e017ae507def2ca417581a52458e29dd6d5f0d
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-02-27 12:20:40 -0300

    fix not show scrolling

--- src/components/ScrollArea.tsx
+++ src/components/ScrollArea.tsx
@@ -12,18 +12,18 @@ export function ScrollArea({ children }: ScrollAreaProps) {
         {children}
       </ScrollAreaContainer.Viewport>
       <ScrollAreaContainer.Scrollbar
-        className="flex select-none touch-none p-[2px] bg-gray-800 rounded-lg transition-colors ease-out hover:bg-gray-700 data-[orientation=vertical]:w-2 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2"
+        className="flex select-none touch-none p-[2px] bg-food-dark-1000 rounded-lg transition-colors ease-out hover:bg-food-dark-800 data-[orientation=vertical]:w-2 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2"
         orientation="vertical"
       >
-        <ScrollAreaContainer.Thumb className="ScrollAreaThumb" />
+        <ScrollAreaContainer.Thumb className="flex-1 bg-food-dark-700 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]" />
       </ScrollAreaContainer.Scrollbar>
       <ScrollAreaContainer.Scrollbar
-        className="flex select-none touch-none p-[2px] bg-gray-800 rounded-lg transition-colors ease-out hover:bg-gray-700 data-[orientation=vertical]:w-2 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2"
+        className="flex select-none touch-none p-[2px] bg-food-dark-1000 rounded-lg transition-colors ease-out hover:bg-food-dark-800 data-[orientation=vertical]:w-2 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2"
         orientation="horizontal"
       >
-        <ScrollAreaContainer.Thumb className="ScrollAreaThumb" />
+        <ScrollAreaContainer.Thumb className="flex-1 bg-food-dark-700 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]" />
       </ScrollAreaContainer.Scrollbar>
-      <ScrollAreaContainer.Corner className="bg-gray-800" />
+      <ScrollAreaContainer.Corner className="bg-food-dark-1000" />
     </ScrollAreaContainer.Root>
   );
 }
