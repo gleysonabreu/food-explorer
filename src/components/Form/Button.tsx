c72a72f27abc7322159e42cb7d54ceaaf9cd693c
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-02-28 17:34:15 -0300

    add Slot to button

--- src/components/Form/Button.tsx
+++ src/components/Form/Button.tsx
@@ -1,13 +1,16 @@
+import { Slot } from "@radix-ui/react-slot";
 import clsx from "clsx";
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: ReactNode;
   isFull?: boolean;
+  asChild?: boolean;
 };
-export function Button({ children, isFull, ...rest }: ButtonProps) {
+export function Button({ children, isFull, asChild, ...rest }: ButtonProps) {
+  const Component = asChild ? Slot : "button";
   return (
-    <button
+    <Component
       {...rest}
       className={clsx(
         "px-8 py-3 flex flex-row items-center justify-center gap-2 bg-food-tints-tomato-100 hover:bg-food-tints-tomato-200 disabled:bg-food-tints-tomato-400 disabled:cursor-not-allowed transition-colors rounded-md text-food-light-100 font-poppins font-medium text-sm focus:outline-none focus:ring-2 focus:ring-food-tints-tomato-100 focus:ring-offset-2 focus:ring-offset-food-dark-400",
@@ -17,6 +20,6 @@ export function Button({ children, isFull, ...rest }: ButtonProps) {
       )}
     >
       {children}
-    </button>
+    </Component>
   );
 }
