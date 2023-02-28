f8058ee7db6506229d49e4b1d368f609b2969139
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-02-28 10:20:56 -0300

    add isFull option to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,20 @@
+import clsx from "clsx";
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: ReactNode;
+  isFull?: boolean;
 };
-export function Button({ children, ...rest }: ButtonProps) {
+export function Button({ children, isFull, ...rest }: ButtonProps) {
   return (
     <button
       {...rest}
-      className="px-8 py-3 flex flex-row items-center justify-center gap-2 bg-food-tints-tomato-100 hover:bg-food-tints-tomato-200 disabled:bg-food-tints-tomato-400 disabled:cursor-not-allowed transition-colors rounded-md text-food-light-100 font-poppins font-medium text-sm focus:outline-none focus:ring-2 focus:ring-food-tints-tomato-100 focus:ring-offset-2 focus:ring-offset-food-dark-400"
+      className={clsx(
+        "px-8 py-3 flex flex-row items-center justify-center gap-2 bg-food-tints-tomato-100 hover:bg-food-tints-tomato-200 disabled:bg-food-tints-tomato-400 disabled:cursor-not-allowed transition-colors rounded-md text-food-light-100 font-poppins font-medium text-sm focus:outline-none focus:ring-2 focus:ring-food-tints-tomato-100 focus:ring-offset-2 focus:ring-offset-food-dark-400",
+        {
+          "w-full": isFull,
+        }
+      )}
     >
       {children}
     </button>
